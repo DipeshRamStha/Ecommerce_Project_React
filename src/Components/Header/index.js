4004dffea15e6ce6b1dfb1ac8f8f6e9f3cad04e9
DipeshRamStha <visit.dipesh933@gmail.com>
2024-07-18 12:05:02 -0500

    -1- App css --> Applied css for the navigation menu -2- Navigation -> index js --> created the Navigation functional component -3- SearchBox -> index js --> created the SearchBox functional component -4- Header -> index js --> Edited the header functional component

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
 import Button from "@mui/material/Button";
 import CountryDropdown from "../CountryDropdown";
-import { IoIosSearch } from "react-icons/io";
 import { FiUser } from "react-icons/fi";
 import { IoBagOutline } from "react-icons/io5";
+import SearchBox from "./SearchBox";
+import Navigation from "./Navigation";
 
 const Header = () => {
   return (
@@ -30,15 +31,7 @@ const Header = () => {
 
               <div className="col-sm-10 d-flex align-items-center part2">
                 <CountryDropdown />
-
-                {/* Header Search Start Here */}
-                <div className="headerSearch ml-3 mr-3">
-                  <input type="text" placeholder="Search for products..." />
-                  <Button>
-                    <IoIosSearch />
-                  </Button>
-                </div>
-                {/* Header Search Ends Here */}
+                <SearchBox />
 
                 <div className="part3 d-flex align-items-center ml-auto">
                   <Button className="circle mr-3">
@@ -60,6 +53,8 @@ const Header = () => {
             </div>
           </div>
         </header>
+
+        <Navigation />
       </div>
     </>
   );
