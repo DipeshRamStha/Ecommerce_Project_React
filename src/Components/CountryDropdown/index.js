3c45996be00b243343ff98bc51ae61538ebcaba7
DipeshRamStha <visit.dipesh933@gmail.com>
2024-07-18 16:15:33 -0500

    -1- App css --> edited the css for the dialog box navigation headerSearch button inside the dialog box locationModal -2- Dropdown -> index js --> Created the Dialog and change the state of the Modal Close or Open -3- Navigation -> index js --> Added the button component inside the navigation menus

--- src/Components/CountryDropdown/index.js
+++ src/Components/CountryDropdown/index.js
@@ -1,10 +1,22 @@
+import React from "react";
 import Button from "@mui/material/Button";
 import { FaAngleDown } from "react-icons/fa6";
+import { Dialog } from "@mui/material";
+import { IoIosSearch } from "react-icons/io";
+import { MdClose } from "react-icons/md";
+import { useState } from "react";
+import Slide from "@mui/material/Slide";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const CountryDropdown = () => {
+  const [isOpenModal, setisOpenModal] = useState(false);
+
   return (
     <>
-      <Button className="countryDrop">
+      <Button className="countryDrop" onClick={() => setisOpenModal(true)}>
         <div className="info d-flex flex-column">
           <span className="label">Your Location</span>
           <span className="name">India</span>
@@ -13,6 +25,83 @@ const CountryDropdown = () => {
           <FaAngleDown />
         </span>
       </Button>
+
+      <Dialog
+        open={isOpenModal}
+        onClose={() => setisOpenModal(false)}
+        className="locationModal"
+        TransitionComponent={Transition}
+      >
+        <h4 className="mb-0">Choose your Delivery Location</h4>
+        <p>Enter your address and we will specify the offer for your area.</p>
+        <Button className="close_" onClick={() => setisOpenModal(false)}>
+          <MdClose />
+        </Button>
+
+        <div className="headerSearch w-100">
+          <input type="text" placeholder="Search your area..." />
+          <Button>
+            <IoIosSearch />
+          </Button>
+        </div>
+
+        <ul className="countryList mt-3">
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>India</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>Sri Lanka</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>Pakistan</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>India</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>Sri Lanka</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>Pakistan</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>India</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>Sri Lanka</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>Pakistan</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>India</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>Sri Lanka</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>Pakistan</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>India</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>Sri Lanka</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>Pakistan</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>India</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>Sri Lanka</Button>
+          </li>
+          <li>
+            <Button onClick={() => setisOpenModal(false)}>Pakistan</Button>
+          </li>
+        </ul>
+      </Dialog>
     </>
   );
 };
