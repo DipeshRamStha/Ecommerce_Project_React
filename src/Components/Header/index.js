6c09e265adc69c43a52d6257e2753127a57706fc
DipeshRamStha <visit.dipesh933@gmail.com>
2024-07-18 22:26:26 -0500

    -1- package-lock json --> Installed the axios for fetching data using GET HTTP Request Method -2- App css --> Updated the the css for location Modal countryList -3- App js --> called the axios get url method and passed the states using createContext -4- CountryDropdown -> index js --> selectetd country display modal completed with substr and dynamically populated countries -5- Header index js --> if context has the country then display the CountryDropdown component

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -6,8 +6,12 @@ import { FiUser } from "react-icons/fi";
 import { IoBagOutline } from "react-icons/io5";
 import SearchBox from "./SearchBox";
 import Navigation from "./Navigation";
+import { useContext } from "react";
+import { MyContext } from "../../App";
 
 const Header = () => {
+  const context = useContext(MyContext);
+
   return (
     <>
       <div className="headerWrapper">
@@ -30,7 +34,8 @@ const Header = () => {
               </div>
 
               <div className="col-sm-10 d-flex align-items-center part2">
-                <CountryDropdown />
+                {context.countryList.length !== 0 && <CountryDropdown />}
+
                 <SearchBox />
 
                 <div className="part3 d-flex align-items-center ml-auto">
